9b169dfe3b083f82d11cea5dd30cb9fff4272533
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-09-19 21:01:05 +0200

    <hi>, <bibl> + <bibl> (même ligne)

--- tools/indent.php
+++ tools/indent.php
@@ -8,7 +8,7 @@ $re_indent = array(
     // Non xr peut être tr^s mêlé
     // '/([^\n]) *(<(xr)>)/' => "$1\n$2",
     // <biblScope>313.38G.</biblScope></bibl></form>
-    '/([^ \n]) *(<(cit|dictScrap|form|sense))/' => "$1\n$2",
+    '/([^ \n]) *(<(cit|dictScrap|form|sense)>)/' => "$1\n$2",
     '/([^ \n]) *(<\/(cit|dictScrap|form|sense)>)/' => "$1\n$2",
     '/(<\/(cit|sense)>) *(<\/(cit|sense)>)/' => "$1\n$3",
     '/(<bibl xml:id=")(\d+)/' => '$1bibl$2',
@@ -16,6 +16,13 @@ $re_indent = array(
     '/([^\.]<\/title>)(<biblScope>)/' => '$1 $2',
     '/\n +/' => "\n",
 );
+
+$re_more = array(
+    '/<hi rend="roman">/' => '<hi>', 
+    '/<hi rend="italic">/' => '<hi>', 
+    '/(<\/bibl>) \+\s*(<bibl)/' => '$1 + $2',
+);
+
 // étape suivante, les chevauchement
 $re_overlap = array(
     // <biblScope>3.<addEnd/>192</biblScope>
@@ -23,41 +30,48 @@ $re_overlap = array(
 
 );
 $dst_file = $src_file;
-$xml = preg_replace(array_keys($re_indent), array_values($re_indent), $xml);
+$xml = preg_replace(array_keys($re_more), array_values($re_more), $xml);
+file_put_contents($dst_file, $xml);
 
-
-$lines = [];
-$separator = "\r\n";
-$l = strtok($xml, $separator);
-
-$tags = array(
-    'body' => true,
-    'cit' => true,
-    'dictScrap' => true,
-    'entry' => true,
-    'form' => true,
-    'sense' => true,
-    'TEI' => true,
-    'teiHeader' => true,
-    'text' => true,
-);
-
-$indent = 0;
-while ($l !== false) {
-    preg_match('/^ *<(\/?)([a-z]+)/i', $l, $matches);
-    $tag = null;
-    if(!isset($matches[2])) {
-        echo $l . "\n";
+/**
+ * Automate spécifique pour indenter le dge, attention, pas très robuste
+ * selon les sauts de lign
+ */
+function indent($xml)
+{
+    $lines = [];
+    $separator = "\r\n";
+    $l = strtok($xml, $separator);
+    
+    $tags = array(
+        'body' => true,
+        'cit' => true,
+        'dictScrap' => true,
+        'entry' => true,
+        'form' => true,
+        'sense' => true,
+        'TEI' => true,
+        'teiHeader' => true,
+        'text' => true,
+    );
+    
+    $indent = 0;
+    while ($l !== false) {
+        preg_match('/^ *<(\/?)([a-z]+)/i', $l, $matches);
+        $tag = null;
+        if(!isset($matches[2])) {
+            echo $l . "\n";
+        }
+        else {
+            $tag = isset($tags[$matches[2]]);
+        }
+        if ($tag && $matches[1]) $indent--;
+    
+        $lines[] = substr("                                ", 0, $indent * 2) 
+        . trim($l);
+        $l = strtok( $separator );
+    
+        if ($tag && !$matches[1]) $indent++;
     }
-    else {
-        $tag = isset($tags[$matches[2]]);
-    }
-    if ($tag && $matches[1]) $indent--;
-
-    $lines[] = substr("                                ", 0, $indent * 2) 
-    . trim($l);
-    $l = strtok( $separator );
-
-    if ($tag && !$matches[1]) $indent++;
+    return $lines;
 }
-file_put_contents($dst_file, implode("\n", $lines));
