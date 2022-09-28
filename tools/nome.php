b9840b5945032622f84f3e2d455e840c53d4a13e
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-09-29 00:59:47 +0200

    Pb unicode

--- tools/nome.php
+++ tools/nome.php
@@ -1,5 +1,6 @@
 <?php
 
+Xdge::tsv_loop();
 
 class Xdge
 {
@@ -7,7 +8,23 @@ class Xdge
     {
         $tsv_file = __DIR__ . '/xdge_nome.tsv';
         $handle = fopen($tsv_file, 'r');
+        $n = 1;
+        echo "n°	file	@xml:id	@type	<orth>	hex <orth> 	hex FORM_C	hex FORM_KC\n";
+
         while (($row = fgetcsv($handle, null, "\t")) !== FALSE) {
+            $form = $row[3];
+            $norm = Normalizer::normalize($form, Normalizer::FORM_C);
+            $norm2 = Normalizer::normalize($form, Normalizer::FORM_KC);
+            if ($form != $norm) {
+                echo $n."\t".implode("\t", $row) 
+                . "\t" . '"' . trim(str_replace('\u', ' ', json_encode($form)), ' "')
+                . "\n" . trim(str_replace('\u', ' ', json_encode($norm)), ' "')
+                . "\n" . trim(str_replace('\u', ' ', json_encode($norm2)), ' "') . '"'
+                . "\n";
+            }
+            $n++;
         }
     }
+
+
 }
